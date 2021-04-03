f751086d1652fb436d5a6e0e37c872de882dd74b
Kami Barut-Wanayo <kamibarut@yahoo.com>
2021-04-03 14:18:27 +0200

    Create 2-2b.php

---
+++ Book exercises/ch2/2-2b.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Framework;
+
+/**
+*
+* PHPDocs ClassProperties Testing Class
+*
+* Class tests only class properties comments
+*
+* @package Exercise
+* @author  Kâmi Barut-Wanayo
+* 
+*/
+
+class Classpropertiestest 
+{
+	
+/**
+*	
+*
+*/
+private $_view1;
+
+/**	
+* @anything
+*/
+private $_view2;
+
+/**
+* @access protected
+* @var string
+*/
+protected $_view3;
+	
+}	
+
+?>

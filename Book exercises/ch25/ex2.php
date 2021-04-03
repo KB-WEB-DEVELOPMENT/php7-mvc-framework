15d02347bc65bef64b7d16e79bfa706e2b49f288
Kami Barut-Wanayo <kamibarut@yahoo.com>
2021-04-03 16:15:01 +0200

    Create ex2.php

---
+++ Book exercises/ch25/ex2.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+
+ch 25 : ex 2 - the following link is a good introduction on how to implement PHPUnit tests in CodeIgniter
+
+link : https://github.com/kenjis/ci-phpunit-test
+
+*/
+
+
+?>

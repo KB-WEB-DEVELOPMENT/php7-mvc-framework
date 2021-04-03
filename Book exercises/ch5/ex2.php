1be05cef4f5bf04a50d0284833b5fc145c2a48f1
Kami Barut-Wanayo <kamibarut@yahoo.com>
2021-04-03 14:24:35 +0200

    Create ex2.php

---
+++ Book exercises/ch5/ex2.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+
+ ex 5-2: Basically, Redis could be added to Memcache as an additional caching mechanism.
+
+ Very few changes need to be added (p.51-60).
+ 
+ For a good introduction to Redis : https://scotch.io/tutorials/getting-started-with-redis-in-php
+ 
+*/
+
+?>

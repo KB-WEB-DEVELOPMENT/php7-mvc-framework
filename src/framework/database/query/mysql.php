179a6f0652b2c95a4cb8b760c44d7d01fe021950
Kami Barut-Wanayo <kamibarut@yahoo.com>
2021-04-04 17:09:11 +0200

    Add files via upload

--- src/framework/database/query/mysql.php
+++ src/framework/database/query/mysql.php
@@ -1,1 +1,33 @@
-test
+<?php
+declare(strict_types=1);
+
+use Framework\Database\Query as Query;
+use Framework\Database\Exception\Sql as Sql;
+
+class Mysql extends Query
+{
+  public function all(): array
+  {
+    $sql = $this->_buildSelect();
+
+    $result = $this->connector->execute($sql);
+
+    if ($result === false)
+
+    {
+
+      $error = $this->connector->lastError;
+      throw new Sql("There was an error with your SQL query: {$error}");
+
+    }
+
+    $rows = array();
+
+    for ($i = 0; $i < $result->num_rows; $i++)
+    {
+      $rows[] = $result->fetch_array(MYSQLI_ASSOC);
+    }
+
+    return $rows;
+  }
+}

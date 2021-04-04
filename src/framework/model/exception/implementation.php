cde025b07b6a65fde8e67377e722d0d71cbb8632
Kami Barut-Wanayo <kamibarut@yahoo.com>
2021-04-04 17:11:59 +0200

    Add files via upload

---
+++ src/framework/model/exception/implementation.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+use Framework\Core\Exception\Implementation as ImplementationB;
+
+class Implementation extends ImplementationB
+{
+        
+}
---
+++ src/framework/model/exception/type.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+use Framework\Model\Exception as Exception;
+
+class Type extends Exception
+{
+
+}

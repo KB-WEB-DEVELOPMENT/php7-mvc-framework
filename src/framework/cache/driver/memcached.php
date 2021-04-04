ca971b5e352a77934f0a4e5d9b891f4ca1fa5468
Kami Barut-Wanayo <kamibarut@yahoo.com>
2021-04-04 16:50:11 +0200

    Add files via upload

--- src/framework/cache/driver/memcached.php
+++ src/framework/cache/driver/memcached.php
@@ -1,1 +1,104 @@
-test
+<?php
+declare(strict_types=1);
+
+use Framework\Cache\Driver as Driver;
+use Framework\Cache\Driver\Memcached as Memcached;
+use Framework\Cache\Exception as Exception;
+
+    class Memcached extends Driver
+    {
+        protected $_service;
+
+        /**
+        * @readwrite
+        */
+        protected string $_host = "127.0.0.1";
+
+        /**
+        * @readwrite
+        */
+        protected string $_port = "11211";
+
+        /**
+        * @readwrite
+        */
+        protected bool $_isConnected = false;
+
+        protected function _isValidService(): bool
+        {
+            $isEmpty = empty($this->_service);
+
+            return ($this->isConnected && $isInstance && !$isEmpty) ? true : false;
+
+        }
+
+        public function connect(): Memcached
+        {
+            try
+            {
+                $this->_service = new Memcached();
+                $this->_service->connect(
+                    $this->host,
+                    $this->port
+                );
+
+                $this->isConnected = true;
+
+                throw new Exception("Unable to connect to a service");
+            }
+            catch (Exception $e)
+            {
+                throw new Exception\Service($e->getMessage());
+            }
+
+            return $this;
+        }
+
+        public function disconnect(): Memcached
+        {
+            if ($this->_isValidService())
+            {
+                $this->_service->close();
+                $this->isConnected = false;
+            }
+
+            return $this;
+        }
+
+        public function get(string $key, $default = null): ?bool
+        {
+            if (!$this->_isValidService())
+            {
+                throw new Exception\Service("Not connected to a valid service");
+            }
+
+            $value = $this->_service->get($key, MEMCACHE_COMPRESSED);
+
+            return $value ? true : $default;
+
+        }
+
+        public function set(string $key, string $value, int $duration = 120): Memcached
+        {
+            if (!$this->_isValidService())
+            {
+                throw new Exception\Service("Not connected to a valid service");
+            }
+
+            $this->_service->set($key, $value, MEMCACHE_COMPRESSED, $duration);
+
+            return $this;
+        }
+
+        public function erase(string $key): Memcached
+        {
+            if (!$this->_isValidService())
+            {
+                throw new Exception\Service("Not connected to a valid service");
+            }
+
+            $this->_service->delete($key);
+
+            return $this;
+        }
+    }
